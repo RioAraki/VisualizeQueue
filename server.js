9f70b80e769c1325801cdc28ddd5c2d362ebc423
RioAraki <yueee.li@mail.utoronto.ca>
2017-09-25 17:14:43 -0400

    add timeline example

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 var http = require('http');
+var fs = require('fs');
 
 function onRequest(request, response) {
-  response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.write('hello world');
-  response.end();
-
+  response.writeHead(200, {'Content-Type': 'text/html'});
+  fs.readFile('./index.html', null, function(error, data){
+    if (error) {
+      response.writeHead(404);
+      response.write('File not found!');
+    } else {
+      reponse.write(data);
+    }
+    response.end();
+  } );
 }
 
 // TODO: change to actual server
