ddfb2a54f05cb3f9da131a89950df8ee5d161ffb
RioAraki <yueee.li@mail.utoronto.ca>
2017-09-26 18:21:43 -0400

    Practice basic routing

--- server.js
+++ server.js
@@ -1,18 +1,19 @@
 var http = require('http');
+var app = require('./app');
 var fs = require('fs');
 
-function onRequest(request, response) {
-  response.writeHead(200, {'Content-Type': 'text/html'});
-  fs.readFile('./index.html', null, function(error, data){
-    if (error) {
-      response.writeHead(404);
-      response.write('File not found!');
-    } else {
-      response.write(data);
-    }
-    response.end();
-  } );
-}
+// function onRequest(request, response) {
+//   response.writeHead(200, {'Content-Type': 'text/html'});
+//   fs.readFile('./index.html', null, function(error, data){
+//     if (error) {
+//       response.writeHead(404);
+//       response.write('File not found!');
+//     } else {
+//       response.write(data);
+//     }
+//     response.end();
+//   } );
+// }
 
 // TODO: change to actual server
-http.createServer(onRequest).listen(8000);
+http.createServer(app.handleRequests).listen(8000);
