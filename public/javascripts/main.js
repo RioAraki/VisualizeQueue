ef6195cffb98847154ffd72c32291e37ac9534b1
RioAraki <yueee.li@mail.utoronto.ca>
2017-10-02 14:30:25 -0400

    add function of search ticket number and fit all

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -5,6 +5,14 @@ var container_kmd = document.getElementById('visualization_kmd');
 
 // Create a DataSet (allows two way data-binding)
 var items_main = new vis.DataSet([
+  {id: 133910, content: '<b><a href="http://ontrack-internal.amd.com/browse/SWDEV-133910" target="_blank">133910 [QR][Maya2018] TDR</b><br/>ASIC: WX7100, OS:RS2</a>', start: '2017-08-29'}
+]);
+
+var items_17_40 = new vis.DataSet([
+  {id: 1, content: '<b><a href="http://ontrack-internal.amd.com/browse/SWDEV-133910" target="_blank">133910 [QR][Maya2018] TDR</b><br/>ASIC: WX7100, OS:RS2</a>', start: '2017-08-29'}
+]);
+
+var items_kmd = new vis.DataSet([
   {id: 1, content: '<b><a href="http://ontrack-internal.amd.com/browse/SWDEV-133910" target="_blank">133910 [QR][Maya2018] TDR</b><br/>ASIC: WX7100, OS:RS2</a>', start: '2017-08-29'}
 ]);
 
@@ -13,5 +21,14 @@ var options = {};
 
 // Create a Timeline
 var timeline_main = new vis.Timeline(container_main, items_main, options);
-var timeline_17_40 = new vis.Timeline(container_17_40, items_main, options);
-var timeline_kmd = new vis.Timeline(container_kmd, items_main, options);
+var timeline_17_40 = new vis.Timeline(container_17_40, items_17_40, options);
+var timeline_kmd = new vis.Timeline(container_kmd, items_kmd, options);
+
+document.getElementById('fit').onclick = function() {
+    timeline_main.fit();
+  };
+
+document.getElementById('find').onclick = function() {
+  //  TODO: error check if the value is not found
+    timeline_main.focus(document.getElementById('find_val').value);
+  };
