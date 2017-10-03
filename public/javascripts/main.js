399c01857774e4a8a472efa4de20f456a2365710
RioAraki <yueee.li@mail.utoronto.ca>
2017-10-03 15:08:11 -0400

    finish the feature of choose timeline to work on

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -11,7 +11,7 @@ var items_main = new vis.DataSet([
 ]);
 
 var items_17_40 = new vis.DataSet([
-  {id: 1, content: '<b><a href="http://ontrack-internal.amd.com/browse/SWDEV-133910" target="_blank">133910 [QR][Maya2018] TDR</b><br/>ASIC: WX7100, OS:RS2</a>', start: '2017-08-29'}
+  {id: 133910, content: '<b><a href="http://ontrack-internal.amd.com/browse/SWDEV-133910" target="_blank">133910 [QR][Maya2018] TDR</b><br/>ASIC: WX7100, OS:RS2</a>', start: '2017-08-29'}
 ]);
 
 var items_kmd = new vis.DataSet([
@@ -23,27 +23,32 @@ var options = {
   editable: false,
   clickToUse: true
 };
-
-// Click to choose a timeline to work on
-$( "body" ).click(function( event ) {
-  console.log( "class: " + event.target.className);
-  console.log( "tag: " + event.target.tagName);
-  console.log( "parent: " + event.target.parentNode.className);
-  console.log( "parent parent: " + event.target.parentNode.parentNode.className);
-  while (event.target.parentNode.tagName == 'div')
-      event.target = event.target.parentNode
-      console.log("after loop class:" + event.target.className)
-      console.log("after loop tag:" + event.target.tagName)
-  if (event.target)
-      console.log("id: " + event.target.idName)
-});
-var choosen = timeline_main
-
-
 // Create a Timeline
 var timeline_main = new vis.Timeline(container_main, items_main, options);
 var timeline_17_40 = new vis.Timeline(container_17_40, items_17_40, options);
 var timeline_kmd = new vis.Timeline(container_kmd, items_kmd, options);
+
+
+var choosen = timeline_main
+// Click to choose a timeline to work on
+$( "body" ).click(function( event ) {
+  console.log("EVENT: " + event.target.innerHTML);
+  if (event.target.innerHTML == 'Mainline'){
+    choosen = timeline_main
+    console.log('mainline')
+  } else if (event.target.innerHTML == '17.40'){
+    choosen = timeline_17_40
+    console.log('17.40')
+  } else if (event.target.innerHTML == 'kmd') {
+    choosen = timeline_kmd
+    console.log('kmd')
+  }
+
+});
+
+
+
+
 
 document.getElementById('fit').onclick = function() {
     timeline_main.fit();
@@ -54,5 +59,5 @@ document.getElementById('fit').onclick = function() {
 document.getElementById('find').onclick = function() {
   //  TODO: error check if the value is not found
 
-    choosen.focus(document.getElementById('find_val').value);
+    timeline_17_40.focus(document.getElementById('find_val').value);
   };
